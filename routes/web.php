b6de7667c8c1302e0c884edc598309ec6e740b46
Leider17 <leidersolano133@gmail.com>
2025-04-08 16:56:34 -0500

    [add] route creation

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\AccessAttemptController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+Route::post('/employees/attempt',[AccessAttemptController::class,'attempt'])->name('accessAttempt.attempt');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/employees/index', [EmployeeController::class, 'index'])->name('employee.index');
+    Route::get('/employees/create',[EmployeeController::class, 'create'])->name('employee.create');
+    Route::post('/employees/store',[EmployeeController::class, 'store'])->name('employee.store');
+    Route::get('/employees/edit/{employee}', [EmployeeController::class,'edit'])->name('employee.edit');
+    Route::put('/employees/update/{employee}', [EmployeeController::class,'update'])->name('employee.update');
+    Route::delete('/employees/destroy/{employee}', [EmployeeController::class,'destroy'])->name('employee.destroy');
+    Route::post('/employees/importCSV', [EmployeeController::class,'importCSV'])->name('employee.importCSV');
+    Route::get('/employees/history/{employee}',[AccessAttemptController::class, 'showHistory'])->name('accessAttempt.history');
+    Route::get('/employees/history/downloadPdf/{employee}',[AccessAttemptController::class, 'downloadPdf'])->name('accessAttempt.downloadPdf');
+});
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
