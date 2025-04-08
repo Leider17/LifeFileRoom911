dfb0394ea424ccccc0f08009f6b3b8ff025eda2a
Leider17 <leidersolano133@gmail.com>
2025-04-08 16:50:12 -0500

    [change] middleware modification to send information to the page

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -51,6 +51,11 @@ class HandleInertiaRequests extends Middleware
                 'location' => $request->url(),
             ],
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
+            'flash' => [
+                'status' => fn () => $request->session()->get('status'),
+                'error' => fn () => $request->session()->get('error'),
+               
+            ],
         ];
     }
 }
